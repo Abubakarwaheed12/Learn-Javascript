f481c012f369e6e79916bf2d6543e05b1dc9ce71
abu <abubakarwaheed6346528@gmail.com>
2022-11-16 15:02:10 +0500

    js

---
+++ Javascript Learnig Code/part1/1.js
@@ -0,0 +1,15 @@
+function myfunc(){
+    document.getElementById("para").innerHTML="my first javascript";
+}
+
+// outputs 
+window.alert("hi this is window alert");
+
+// variables 
+
+let x , y ,z ;
+x=5;
+y=4;
+z=x+y;
+
+document.write(z);
