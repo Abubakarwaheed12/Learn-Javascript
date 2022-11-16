e7e5f5e2772fe69f3e00ab5c8de6493754144651
abu <abubakarwaheed6346528@gmail.com>
2022-11-16 13:19:09 +0500

    document

--- Part1/intro.js
+++ Part1/intro.js
@@ -1,2 +1,6 @@
 // here  we will practice all over the javascript 
 console.log("hello world abu bakar the web programmer is here for solving your problems in efficeient way");
+// the above code run in console 
+
+document.write("hello world abu bakar the web programmer is here for solving your problems in efficeient way")
+// the above code run in browser
