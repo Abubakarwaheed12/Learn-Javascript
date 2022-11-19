a5c4f21e95dcd38799b0139daad3b113b05ca991
abu <abubakarwaheed6346528@gmail.com>
2022-11-19 11:45:23 +0500

    oops in js

---
+++ Javascript Learnig Code/part1/oops.js
@@ -0,0 +1,15 @@
+
+
+// OOP in Javascript 
+
+//  create a function that create user and return object 
+
+let user1={
+    name:"abu bakar",
+    last_name:"waheed",
+    address:"muridkey",
+    age:22,
+    about(){
+        console.log(`${this.name} ${this.last_name}`);
+    }
+}
